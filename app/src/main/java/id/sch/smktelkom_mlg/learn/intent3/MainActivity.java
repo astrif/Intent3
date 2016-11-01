eee5191647fb245d96f36f6a4678e14cdc4d0f9d
Astrif <fadilahastri21@gmail.com>
2016-11-01 10:36:21 +0700

    Intent Browser

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -30,9 +30,26 @@ public class MainActivity extends AppCompatActivity {
                     {
                         composeSmsMessage("Pesan dari SMK Telkom Malang");
                     }
+                });
+        findViewById(R.id.imageViewBrowser)
+                .setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View view)
+                    {
+                        openWebPage("https://www.smktelkom-mlg.sch.id/");
+                    }
                 }
-
+                
+                
                 );
+    }
+
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) !=null)
+            startActivity(intent);
     }
 
     public void composeSmsMessage(String message) {
